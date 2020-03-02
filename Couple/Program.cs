9ad2fadda890a429f7f1959c08bef91d5c424ed0
grkapoor <59718600+grkapoor@users.noreply.github.com>
2020-03-02 19:55:43 +0800

    Add files via upload

---
+++ Couple/Program.cs
@@ -0,0 +1,51 @@
+﻿//
+// Test program for the Couple class.
+//
+// Author: Dr. Nicholas Sheppard
+//
+
+using System;
+
+namespace Couple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// get the couple
+            Couple c1 = Couple.GetInstance(1);
+			Couple c2 = Couple.GetInstance(2);
+			
+			// print out some diagnostic information
+			if (c1 != null && c2 != null) {
+				Console.WriteLine("Couple.GetInstance(1) returns " + c1.GetHashCode());
+				Console.WriteLine("Couple.GetInstance(2) returns " + c2.GetHashCode());
+			} else {
+				Console.WriteLine("Couple.GetInstance() did not return an instance.");
+				return;
+			}
+			
+			// check that it really is a couple
+			bool ok = true;
+			Couple newc1 = Couple.GetInstance(1);
+			Couple newc2 = Couple.GetInstance(2);
+			if (c1 != newc1) {
+				Console.WriteLine("Couple.GetInstance(1) returned a different instance " + newc1.GetHashCode());
+				ok = false;
+			}
+			if (c2 != newc2) {
+				Console.WriteLine("Couple.GetInstance(2) returned a different instance " + newc2.GetHashCode());
+				ok = false;
+			}
+			if (c1 == c2) {
+				Console.WriteLine("Couple.GetInstance(1) and Couple.GetInstance(2) return the same instance.");
+				ok = false;
+			}
+			
+			if (ok) {
+				Console.WriteLine("Tests passed.");
+			}
+			Console.ReadLine();
+        }
+    }
+}
