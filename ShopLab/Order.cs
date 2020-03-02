7bb4a00818ff65a1cb6934fcc33be522122325b9
grkapoor <59718600+grkapoor@users.noreply.github.com>
2020-03-02 19:52:02 +0800

    Add files via upload

---
+++ ShopLab/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopLab
+{
+    class Order : IOrder
+    {
+        public Order() { }
+        public Order(decimal totalCost, decimal weightInKG)
+        {
+            TotalCost = totalCost;
+            WeightInKG = weightInKG;
+            System.Console.WriteLine("Creating an order with cost " + this.TotalCost + " and weight " + this.WeightInKG);
+        }
+        public decimal TotalCost { get; set; }
+        public decimal WeightInKG { get; set; }
+        
+    }
+}
---
+++ ShopLab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShopLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order myOrder = new Order(10, 20);
+                 
+            
+           
+        }
+    }
+}
