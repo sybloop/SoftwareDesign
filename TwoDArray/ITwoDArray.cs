61eb924e18c2bac3283eb25b799159166adf41aa
grkapoor <59718600+grkapoor@users.noreply.github.com>
2020-03-12 23:54:23 +0800

    Add files via upload

---
+++ TwoDArray/ITwoDArray.cs
@@ -0,0 +1,28 @@
+﻿//
+// ICT2106 Software Design - 2D array example
+//
+// Interface for two-dimensional arrays.
+//
+// Author: Nicholas Sheppard
+//
+
+namespace TwoDArray
+{
+    interface ITwoDArray<T>
+    {
+        // get an iterator through the array
+        ITwoDIterator<T> CreateRowMajorIterator();
+
+        // get the element at position (i, j)
+        T Get(int i, int j);
+
+        // set the element at position (i, j)
+        void Set(int i, int j, T elem);
+
+        // get the number of rows in the array
+        int Rows();
+
+        // get the number of columns in the array
+        int Columns();
+    }
+}
---
+++ TwoDArray/TwoDRowMajorIteratorGeneric.cs
@@ -0,0 +1,49 @@
+﻿//
+// ICT2106 Software Design - 2D array example
+//
+// Concrete row-major iterator for two-dimensional arrays.
+//
+// Author: Nicholas Sheppard
+//
+namespace TwoDArray
+{
+    class TwoDRowMajorIteratorGeneric<T> : ITwoDIterator<T>
+    {
+        // the collection being iterated through
+        TwoDArrayGeneric<T> collection;
+
+        // counters
+        private int i, j;
+
+        // constructor
+        public TwoDRowMajorIteratorGeneric(TwoDArrayGeneric<T> collectionIn)
+        {
+            collection = collectionIn;
+            i = 0;
+            j = 0;
+        }
+
+        // get the current element of the iteration
+        public T Current()
+        {
+            return collection.Get(i, j);
+        }
+
+        // move to the next element of the iteration
+        public void Next()
+        {
+            j++;
+            if (j >= collection.Columns())
+            {
+                j = 0;
+                i++;
+            }
+        }
+
+        // test whether or not the iteration has finished
+        public bool IsDone()
+        {
+            return i >= collection.Rows();
+        }
+    }
+}
