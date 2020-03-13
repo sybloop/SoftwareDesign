eaa895f62b55a91a3a0622063bec8b38855dc808
grkapoor <59718600+grkapoor@users.noreply.github.com>
2020-03-13 12:59:43 +0800

    Add files via upload

---
+++ ConstantDictionary/IConstantDictionary.cs
@@ -0,0 +1,20 @@
+﻿//
+// ICT2106 Software Design
+//
+// Interface to be relased by constant dictionaries.
+//
+using System.Collections.Generic;
+
+namespace ICT2106.ConstantDictionary
+{
+    interface IConstantDictionary<K, V>
+    {
+        bool ContainsKey(K key);
+        bool ContainsValue(V value);
+        bool Equals(object o);
+        Dictionary<K, V>.Enumerator GetEnumerator();
+        int GetHashCode();
+        string ToString();
+        bool TryGetValue(K key, out V defaultValue);
+    }
+}
---
+++ ConstantDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿//
+// ICT2106 Software Design
+//
+// Program for testing constant dictionaries
+//
+using System;
+using System.Collections.Generic;
+
+namespace ICT2106.ConstantDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a test dictionary
+            IDictionary<int, String> mutableDict = new Dictionary<int, String>();
+            mutableDict.Add(1, "cricket");
+            mutableDict.Add(2, "mosquito");
+            mutableDict.Add(3, "fly");
+
+            // create an immutable dictionary
+            IConstantDictionary<int, String> immutableDict = new ConstantDictionaryAdapter<int, String>(mutableDict);
+
+            // print the contents of the dictionary
+            foreach (KeyValuePair<int, String> keypair in immutableDict)
+            {
+                Console.WriteLine(keypair.ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
